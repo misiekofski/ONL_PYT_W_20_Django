bfe36186ed60d79e4ecef3243870516ef48934d5
Michal Dobrzycki <dobrzycki@gmail.com>
2022-08-27 11:07:56 +0200

    Add first view

--- ONL_PYT_W_20_Django/urls.py
+++ ONL_PYT_W_20_Django/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from day_one.views import show_number
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/<int:number>', show_number),
 ]
--- day_one/views.py
+++ day_one/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def show_number(request, number):
+    answer = f"""
+    <html>
+        <body>
+        <p>The answer is {number}!</p>
+        </body>
+    </html>
+    """
+
+    return HttpResponse(answer)
