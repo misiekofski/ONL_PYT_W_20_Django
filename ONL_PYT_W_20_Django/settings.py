90b00dcd7b41228ae6d3782c42c215addde69b37
Michal Dobrzycki <dobrzycki@gmail.com>
2022-08-27 11:23:10 +0200

    Remove secret from settings.py

--- ONL_PYT_W_20_Django/settings.py
+++ ONL_PYT_W_20_Django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!=vqdz=pj4hsvdl@xk&gccznyb5m8ggiwiu_nbyl1wnk4&kg__'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
